f6fa18bee1d5d529a274e0b039c229b12585cf16
Sai Aryan Goswami <111999256+SaiAryan1784@users.noreply.github.com>
2023-06-17 18:33:02 +0530

    Initial commit

---
+++ nft-minter-dapp-main/hardhat.config.js
@@ -0,0 +1,20 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+
+
+module.exports = {
+  networks: {
+    hardhat: {
+    },
+    liberty: {
+      url: "https://liberty20.shardeum.org/",
+      chainId: 8081,
+      accounts:[``]
+    },
+    sphinx: {
+        url: "https://sphinx.shardeum.org/",
+        accounts: [``]
+        }
+  },
+  solidity: "0.8.3",
+};
